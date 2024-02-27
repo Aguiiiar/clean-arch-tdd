ae795368c63890e8b533648c4b9622091466cca6
Aguiiiar <devaguiar14@gmail.com>
2024-02-27 01:18:36 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Bory Parser Middleware', () => {
+  it('should oarse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({
+        name: 'John Doe'
+      })
+      .expect({
+        name: 'John Doe'
+      })
+  })
+})
