d36af62d3319f0332e154f8a8f8870ec1bd51f59
Aguiiiar <devaguiar14@gmail.com>
2024-02-27 01:39:07 -0300

    feat: ensure requests enable cors

---
+++ src/main/middlewares/cors.ts
@@ -0,0 +1,13 @@
+import { type NextFunction, type Request, type Response } from 'express'
+
+export const cors = (
+  _request: Request,
+  response: Response,
+  next: NextFunction
+): void => {
+  response.set('access-control-allow-origin', '*')
+  response.set('access-control-allow-methods', '*')
+  response.set('access-control-allow-headers', '*')
+
+  next()
+}
